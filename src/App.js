207f3dca5b36a6f99a0c64faa1c2e525c7bdad7f
aishah849 <aishahalrashid@gmail.com>
2022-07-11 23:50:19 +0800

    Removed all CSS stylings and added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Aishah Al-Rashid and is {""}
+        <a
+          href="https://github.com/aishah849/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
